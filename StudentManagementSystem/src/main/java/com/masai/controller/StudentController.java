ffee1364384e4560f71b5195ce78e4609d389b52
Rohit_Ramawat <119414002+Rohit-Ramawat@users.noreply.github.com>
2023-12-05 15:58:28 +0530

    Update StudentController.java

--- StudentManagementSystem/src/main/java/com/masai/controller/StudentController.java
+++ StudentManagementSystem/src/main/java/com/masai/controller/StudentController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.masai.model.Student;
 import com.masai.service.StudentService;
 
+@CrossOrigin(origins = "http://127.0.0.1:5500/index.html")
 @RestController
 public class StudentController {
 	
